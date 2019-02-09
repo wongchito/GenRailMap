dd0ef84d546e7fe8b545fdd517047146d38871ca
Chito Wong <9390786+wongchito@users.noreply.github.com>
2019-02-09 02:05:23 +0000

    Show passed section

--- draw.js
+++ draw.js
@@ -10,6 +10,7 @@ function setSVGSize() {
 
     var pad = document.getElementById('padding_input').value;
     setLineLength(width, pad);
+    reposStn();
 }
 
 function setPadding(src) {
@@ -24,44 +25,35 @@ function setPadding(src) {
 
     var svg_width = document.getElementById('root').getAttribute('width');
     setLineLength(svg_width, pad);
+    reposStn();
 }
 
 function setLineLength(svg_width, pad) {
     var lineStart = svg_width * pad / 200;
     var lineLength = svg_width * (1 - pad/100);
-    document.getElementById('mainLine').setAttribute('d', 'M ' + lineStart.toString() + ',30 h ' + lineLength.toString());
+    document.getElementById('line_main').setAttribute('d', 'M ' + lineStart.toString() + ',30 h ' + lineLength.toString());
 }
 
-function setLineColour(elem) {
+function setLineColour() {
     var city = document.getElementById('city').value;
     var lineName = document.getElementById(city).value;
 
-    elem.setAttribute('class', 'c'+lineName);
-
-    // var root = document.getElementById('root');
-    // rootNS = root.namespaceURI;
-    // var lines = document.createElementNS(rootNS, 'g');
-    // lines.setAttribute('class', 'LineDiagram');
-    // root.appendChild(lines);
-    // var line = document.createElementNS(rootNS, 'path');
-    // lines.appendChild(line)
-    // line.setAttribute('class', 'c'+lineName);
-    // line.setAttribute('d', 'M 0,20 H 100');
-}
-
-function changeColour(elem) {
-    elem.setAttribute('class', 'cgz2');
+    document.getElementById('line_main').setAttribute('class', 'c'+lineName);
 }
 
 function getCity(dropdown) {
     var value = dropdown.options[dropdown.selectedIndex].value;
-    if (value == 'hk') {
-        document.getElementById('hk').style.display = 'block';
-        document.getElementById('gz').style.display = 'none';
-    }
-    if (value == 'gz') {
-        document.getElementById('hk').style.display = 'none';
-        document.getElementById('gz').style.display = 'block';
+
+    var cities = document.getElementById('cities').children;
+
+    for (i=0; i<cities.length; i++) {
+        if (cities[i].getAttribute('id') == value) {
+            cities[i].style.display = 'block'
+            var lineName = cities[i].children[0].value;
+            document.getElementById('line_main').setAttribute('class', 'c'+lineName);
+        } else {
+            cities[i].style.display = 'none'
+        }
     }
 }
 
@@ -78,9 +70,11 @@ function addStn(elem) {
     stn_icon.parentNode.insertBefore(new_stn_icon, stn_icons[parseInt(add_index)]);
 
     reindexStn();
+    reposStn();
+    redrawLinePassed();
 }
 
-function removeStn(elem) {
+function removeStn(elem) { 
     // Remove station in HTML
     var par = elem.parentNode;
     var removed_index = par.getAttribute('id').substring(3);
@@ -91,6 +85,19 @@ function removeStn(elem) {
     stn_icon.parentNode.removeChild(stn_icon);
     
     reindexStn();
+    reposStn();
+
+    // Fix radio input
+    var current_stn_index = document.querySelector('input[name="current"]:checked');
+    if (current_stn_index == null) {
+        var n_stn = document.getElementById('stn_list').children.length;
+        if (removed_index >= n_stn) {
+            removed_index = n_stn - 1;
+        }
+        document.getElementById('stn_list').children[removed_index].children[0].checked = true;
+    }
+
+    redrawLinePassed();
 }
 
 function reindexStn() {
@@ -104,15 +111,59 @@ function reindexStn() {
 
     var stn_icons = document.getElementById('stations').children;
 
+    // Re-index stations in SVG
+    for (i=0; i<n_stn; i++) {
+        stn_icons[i].setAttribute('id', 'stn_icon_'+i.toString())
+    }
+}
+
+function reposStn() {
+    var stn_icons = document.getElementById('stations').children;
+    var n_stn = stn_icons.length;
+
     var svg_width = document.getElementById('root').getAttribute('width');
     var pad = document.getElementById('padding_input').value;
     var lineStart = svg_width * pad / 200;
     var lineLength = svg_width * (1 - pad/100);
 
-    // Re-index stations in SVG
     for (i=0; i<n_stn; i++) {
         var stn_icons_x = lineStart + (lineLength / (n_stn-1)) * i;
-        stn_icons[i].setAttribute('id', 'stn_icon_'+i.toString())
         stn_icons[i].setAttribute('transform', 'translate(' + stn_icons_x + ',30)')
     }
+}
+
+function setDirection(elem) {
+    var direction = elem.value;
+    redrawLinePassed(null, direction);
+}
+
+function setCurrentStn(elem) {
+    var current_stn_index = elem.parentNode.getAttribute('id').substring(3);
+    redrawLinePassed(current_stn_index, null);
+}
+
+function redrawLinePassed(current_stn_index=null, direction=null) {
+    if (current_stn_index == null) {
+        var current_stn_index = document.querySelector('input[name="current"]:checked').parentNode.getAttribute('id').substring(3);
+    }
+    if (direction == null) {
+        var direction = document.querySelector('input[name="direction"]:checked').value;
+    }
+
+    var svg_width = document.getElementById('root').getAttribute('width');
+    var pad = document.getElementById('padding_input').value;
+    var lineStart = svg_width * pad / 200;
+    var lineLength = svg_width * (1 - pad/100);
+    var lineEnd = lineStart + lineLength;
+    var n_stn = document.getElementById('stations').children.length;
+    var current_stn_x = lineStart + (lineLength / (n_stn-1)) * current_stn_index;
+
+    if (direction == 'right') {
+        var path_str = 'M ' + current_stn_x.toString() + ',30 H ' + lineStart.toString();
+    }
+    if (direction == 'left') {
+        var path_str = 'M ' + current_stn_x.toString() + ',30 H ' + lineEnd.toString();
+    }
+
+    document.getElementById('line_passed').setAttribute('d', path_str);
 }
