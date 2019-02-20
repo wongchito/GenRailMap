16d44bbbbb78272d022a9975ebdfeac7f678adb5
Chito Wong <9390786+wongchito@users.noreply.github.com>
2019-02-20 23:01:02 +0100

    Add font list

---
+++ fonts.js
@@ -0,0 +1,13 @@
+var fontZH = [
+    "Hiragino Mincho ProN", 
+    "Source Han Serif", 
+    "Noto Serif CJK JP",
+    "STHeiti",
+    "sans-serif"
+]
+
+var fontEN = [
+    "Myriad Pro", 
+    "Arial", 
+    "sans-serif"
+]
